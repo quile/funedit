333c5d07298fb3b2ea7f56a18274d02c1ce8706d
Kyle Dawkins <quile@users.noreply.github.com>
2015-09-15 15:10:06 -0700

    Second milestone: a running server.

--- funedit.js
+++ funedit.js
@@ -8,4 +8,21 @@ if (filename) {
     console.log(file.contents().toString());
 }
 
+// TODO: manage dependency injection better with lifecycle/ecosystem code
 
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(6789);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
