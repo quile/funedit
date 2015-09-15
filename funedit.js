e7c93118cede5c65fd0629840bb60d9ff582f660
Kyle Dawkins <quile@users.noreply.github.com>
2015-09-15 15:14:12 -0700

    Thrid milestone: static assets.

--- funedit.js
+++ funedit.js
@@ -10,11 +10,14 @@ if (filename) {
 
 // TODO: manage dependency injection better with lifecycle/ecosystem code
 
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var express = require('express');
+var app     = express();
+var server  = require('http').Server(app);
+var io      = require('socket.io')(server);
 
 server.listen(6789);
+
+app.use(express.static("public"));
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
